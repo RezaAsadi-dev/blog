1ea089dd0e2514849c5da3714ea9611a5783564c
Reza Asadi <rezaasadii.dev@gmail.com>
2025-04-20 21:17:27 +0330

    Update Next.js configuration to include Unsplash as an additional image domain for improved media sourcing.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     images: {
-      domains: ['nabsteel.rahkartest.ir'],
-    },};
+  images: {
+    domains: ['nabsteel.rahkartest.ir', 'images.unsplash.com'],
+  },
+};
 
 export default nextConfig;
- 
