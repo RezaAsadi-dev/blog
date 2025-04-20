a6f1bdd421909f60da0995d52a78989333d916a1
Reza Asadi <rezaasadii.dev@gmail.com>
2025-04-20 21:12:52 +0330

    Replace Unsplash image URLs with a default image fallback in Posts and PostDetails pages for improved media handling.

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -93,7 +93,7 @@ export default function PostDetailsPage({
                   <Image
                     src={
                       data?.featured_media_object?.source_url ||
-                      "https://images.unsplash.com/photo-1614687153862-b0e115ebcef1?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      "/images/default-post.jpg"
                     }
                     alt={`Image for post titled "${data?.title?.rendered}"`}
                     width={500}
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -120,7 +120,7 @@ export default function PostsPage() {
                     className="z-0 w-full h-full object-cover"
                     src={
                       post.featured_media_object?.source_url ||
-                      "https://images.unsplash.com/photo-1614687153862-b0e115ebcef1?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      "/images/default-post.jpg"
                     }
                     priority
                   />
